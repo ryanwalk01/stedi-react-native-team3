23f1ba5217777fadf8979b37b9689e63a4ed0810
ryanwalk01 <ryan.walk01@gmail.com>
2023-05-11 11:21:24 -0600

    Fixed Ryan function

--- Login.js
+++ Login.js
@@ -60,7 +60,7 @@ export function HelloGrant(props){
 }
 
 // make a button that will display "Hello Ryan" when pressed
-export function HelloGrant(props){
+export function HelloRyan(props){
     return(
         <View style={styles.login}>
             <Text>Hello Ryan</Text>
